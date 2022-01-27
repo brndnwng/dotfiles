aa4fbe62bfd219d5728a0d9545513dbee002eeb7
Brandon Wong <brandon.wong@shopify.com>
2022-01-27 15:32:25 -0500

    fix typo

--- .profile_utils.sh
+++ .profile_utils.sh
@@ -1,6 +1,6 @@
 urls () {
  host=$(cat /etc/spin/machine/fqdn)
  echo "graphQL: https://app.shopify.$host/services/internal/shops/1/graphql"
- echo "shop1: https://shop1.app.shopify.$host/admin"
+ echo "shop1: https://shop1.shopify.$host/admin"
  echo "db: shopify.$host"
 }
