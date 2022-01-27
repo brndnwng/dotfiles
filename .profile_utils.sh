aa1c4749d4e6269008b3e51dcc2561739892b95d
Brandon Wong <brandon.wong@shopify.com>
2022-01-27 15:29:41 -0500

    Add more urls

--- .profile_utils.sh
+++ .profile_utils.sh
@@ -1,4 +1,6 @@
-dev_urls () {
+urls () {
  host=$(cat /etc/spin/machine/fqdn)
  echo "graphQL: https://app.shopify.$host/services/internal/shops/1/graphql"
+ echo "shop1: https://shop1.app.shopify.$host/admin"
+ echo "db: shopify.$host"
 }
